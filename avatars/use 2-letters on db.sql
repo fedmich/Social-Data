1b3007d67a0ea1f976f1996263c4ca61623c10e7
fed <fedmich@gmail.com>
2024-11-03 08:39:30 +0800

    SQL for 2 Letters A-Z

---
+++ avatars/use 2-letters on db.sql
@@ -0,0 +1,8 @@
+UPDATE users
+SET picture = CONCAT(
+    'https://raw.githubusercontent.com/fedmich/Social-Data/refs/heads/main/avatars/letter-names-2/',
+    LOWER(SUBSTRING(first_name, 1, 1)),
+    LOWER(SUBSTRING(last_name, 1, 1)),
+    '.png'
+)
+WHERE picture LIKE '%/username?username%';
